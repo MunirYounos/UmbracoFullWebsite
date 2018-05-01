e966e15f2d9992a4a38abe721c9f6d781757c421
Munir Younosi <eaaamy@gmail.com>
2018-05-01 11:15:40 +0200

    First commit of all files

---
+++ AWebDev/AWebDev/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AWebDev.ViewModels;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+
+namespace AWebDev.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactFormPartial", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            else
+            {
+                //send message
+                MailMessage message = new MailMessage();
+                message.To.Add("neeewbi@gmail.com");
+                message.Subject = model.Subject;
+                message.From = new MailAddress(model.Email, model.Name);
+                message.Body = model.Message;
+
+
+                //Messages 
+                IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+                // assign values
+                comment.SetValue("messagename", model.Name);
+                comment.SetValue("email", model.Email);
+                comment.SetValue("subject", model.Subject);
+                comment.SetValue("messagecontent", model.Message);
+                // save to Umbraco
+
+                Services.ContentService.Save(comment);
+                // Services.ContentService.SaveAndPublish(comment);
+
+
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.EnableSsl = true;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.Credentials = new System.Net.NetworkCredential("neeewbi@gmail.com", "2NeeebiNeeebi");
+                    smtp.EnableSsl = true;
+                    // send mail
+                    smtp.Send(message);
+
+                    TempData["success"] = true;
+                    return RedirectToCurrentUmbracoPage();
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ AWebDev/AWebDev/ViewModels/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace AWebDev.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please insert a valid Email Address")]
+        [Display(Name ="Email")]
+        [RegularExpression(@"^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,6})+$", 
+         ErrorMessage ="Please insert a correct email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter a subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage ="Please write your message")]
+        public string Message { get; set; }
+    }
+}
